3b09c2a3d9f66bfb0aa8bcd864f970e4678a1d2e
Lucas Alves Marques <alves@ibfc.org.br>
2026-01-19 16:34:38 -0300

    feat: adiciona função de atualizar tarefa

--- Fase 1/Semana 3/Dia 5/src/Services/tarefasServises.ts
+++ Fase 1/Semana 3/Dia 5/src/Services/tarefasServises.ts
@@ -1,5 +1,6 @@
 import { Tarefa } from "../Models/tarefa";
 import { criarTarefaDTO } from "../DTOs/dtoTarefa";
+import { atualizarTarefaDTO } from "../DTOs/dtoTarefa";
 
 let banco: Tarefa[] = [];
 
@@ -16,5 +17,17 @@ export function criarTarefa(dados: criarTarefaDTO): Tarefa {
 export function listarTarefas(): Tarefa[]{
 
     return banco;
+
+}
+
+export function atualizarTarefa(dados: atualizarTarefaDTO): atualizarTarefaDTO | null {
+
+    const index = banco.findIndex(t => t.id === dados.id);
+  
+    if (index === -1) return null;
+  
+    banco[index] = { ...banco[index], ...dados };
+  
+    return banco[index];
 
 }
--- Fase 1/Semana 3/Dia 5/src/index.ts
+++ Fase 1/Semana 3/Dia 5/src/index.ts
@@ -1,8 +1,14 @@
-import {criarTarefa, listarTarefas } from "./Services/tarefasServises";
+import {criarTarefa, atualizarTarefa ,listarTarefas } from "./Services/tarefasServises";
 
 criarTarefa({ tarefa: 'Regar as plantas', data: '19/01/2026' })
 criarTarefa({ tarefa: 'Começar nova Fase do Projeto', data: '19/01/2026' });
 
 console.log('\nTarefas Criadas');
+
+console.log(listarTarefas());
+
+atualizarTarefa({id: 2, data: '20/01/2026'})
+
+console.log('\nTarefa Atualizada');
 
 console.log(listarTarefas());
